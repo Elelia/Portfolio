e0361a0420f9a148146d1bfcda894df20b733bbb
Elelia <llucas@ensup.eu>
2024-10-06 15:31:08 +0200

    add hero and header section

---
+++ src/components/Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <nav>
+      <div>
+        <h1>Lisa Lucas</h1>
+      </div>
+      <ul>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#'>About</a>
+        </li>
+        <li>
+          <a href='#'>Experience</a>
+        </li>
+        <li>
+          <a href='#'>Projects</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+      </ul> 
+    </nav>
+  )
+}
+
+export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { DEFAULT_CIPHERS } from "tls";
 
 const config: Config = {
   content: [
@@ -11,6 +12,26 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        black: {
+          DEFAULT: "#000",
+          100: '#000319'
+        }
+
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
   },
